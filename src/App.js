6a2341e90bb114e75e0887bb9183603d7d72b2ce
Gopal-Swami <Gopalswami8619@gmail.com>
2021-11-09 20:56:49 +0530

    Version 0.1
    Setup Initiated in local

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Navbar from "./Components/Navbar.jsx";
 import TextForm from "./Components/TextForm.jsx";
+import AboutTextUtils from "./Components/AboutTextUtils";
 export default function App() {
   return (
     <>
       <Navbar title="TextUtils" />
-      <TextForm />
+      {/* <TextForm /> */}
+      <AboutTextUtils/>
     </>
   );
 }
---
+++ src/Components/AboutTextUtils.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutTextUtils = () => {
+    return (
+        <div className="container my-3">
+            hello
+        </div>
+    )
+}
+
+export default AboutTextUtils
